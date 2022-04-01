467f684732b2ab2cbed3dec9ebe876a912d98728
Артем Семёнов <87887874+artart76@users.noreply.github.com>
2022-04-01 17:31:02 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,48 @@
+const quizData = [{
+		number: 1,
+		title: "test question",
+		answer_alias: "money",
+		answers: [{
+				answer_title: "1",
+				type: "checkbox"
+			},
+			{
+				answer_title: "2",
+				type: "checkbox"
+			},
+			{
+				answer_title: "my answer",
+				type: "text"
+			}
+		]
+	},
+	{
+		number: 2,
+		title: "test question 2",
+		answer_alias: "great",
+		answers: [{
+				answer_title: "element 1",
+				type: "radio"
+			},
+			{
+				answer_title: "element 2",
+				type: "radio"
+			},
+			{
+				answer_title: "element 3",
+				type: "radio"
+			}
+		]
+	},
+	{
+		number: 3,
+		title: "Your private data",
+		answer_alias: "phone",
+		answers: [{
+			answer_title: "Your number phone",
+			type: "text"
+		},
+		]
+	}
+];
+
